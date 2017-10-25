f32b2e1b5d5f1f5e35b5fbc23ca6a65555184c33
Dillion Verma <dillionverma@hotmail.com>
2017-10-24 23:16:54 -0400

    remove cout from pointers

--- pointers/pointers.cc
+++ pointers/pointers.cc
@@ -3,37 +3,26 @@
 using namespace std;
 
 int main() {
-  cout << "A pointer is a data type which stores an address." << endl;
-  cout << "To declare that a variable is a pointer, just add * in definition" << endl;
-  cout << "Address of variable is denoted by &" << endl;
-  cout << endl;
+  // A pointer is a data type which stores an address. 
+  // To declare that a variable is a pointer, just add * in definition
+  // Address of variable is denoted by &
 
   int x = 5;
-  cout << "int x = 5" << endl;
-  int *p = &x; 
-  cout << "int *p = &x; // int pointer which holds address of variable x" << endl;
-  cout << endl;
-  
-  cout << "x: " << x << endl;
-  cout << "&x: " << &x << endl;
-  cout << "p: " << p << endl;
-  cout << "*p: " << *p << endl;
-  cout << "// NOTE: *p is an alias for x" << endl;
-  cout << endl;
+  int *p = &x;  // int pointer which holds address of variable x 
 
-  x = x + 5;
-  cout << "x = x + 5: " << x << endl;
-  x = *p + 5;
-  cout << "x = *p + 5: " << x << endl;
-  *p = *p + 5;
-  cout << "*p = *p + 5: " << x << endl;
-  cout << endl;
+  // NOTE: *p is an alias for x
 
-  cout << "*p: " << *p << endl;
-  cout << "x: " << x << endl;
-  cout << endl;
+  x = x + 5;    // 10
+  x = *p + 5;   // 15
+  *p = *p + 5;  // 20
 
-  cout << "& and * operator cancel each other out: " << endl;
-  cout << "&*p: " << &*p << endl;
+  //notice how (x == *p)
+  cout << x << endl;
+  cout << *p << endl;
+
+  // & and * operator cancel each other out: 
+  cout << *&p << endl;
+  cout << p << endl;
+
   return 0;
 }
