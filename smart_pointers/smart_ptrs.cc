01a7b89396553cc44ba637c88f0ae607295070f0
Dillion Verma <dillionverma@hotmail.com>
2017-12-10 23:19:35 -0500

    add unique_ptr class definition

--- smart_pointers/smart_ptrs.cc
+++ smart_pointers/smart_ptrs.cc
@@ -1,7 +1,26 @@
 #include <iostream>
-#include <memory>
 
 using namespace std;
+
+// Here is definition of unique_ptr class. Alternative would be to include <memory> to get 
+//  STL unique_ptr definition
+
+template<typename T> class my_unique_ptr {
+    T *ptr;
+  public:
+    explicit my_unique_ptr(T *p):ptr{p}{}
+    ~my_unique_ptr() { delete ptr; }
+    my_unique_ptr(const my_unique_ptr &) = delete;
+    my_unique_ptr &operator=(const my_unique_ptr &) = delete;
+    my_unique_ptr(my_unique_ptr &&other):ptr{other.ptr} { other.ptr = nullptr; }
+    my_unique_ptr &operator=(my_unique_ptr &&other) {
+      std::swap(ptr, other.ptr);
+      return *this;
+    }
+    T &operator*() { return *ptr; }
+};
+
+
 
 int main() {
   // C++ RAII - Resource Acquisition Is Initialization Idiom
@@ -12,13 +31,11 @@ int main() {
   //   the creation and deletion of anything heap allocated
   // Smart pointers can be accessed using <memory> header
   
-  // 3 different methods to create smart pointers
-  unique_ptr<int> p{ new int(5) };
-  unique_ptr<int> q = make_unique<int>(6);
+  // 2 different methods to create smart pointers
+  my_unique_ptr<int> p{ new int(5) };
   auto r = make_unique<int>(7); 
 
   cout << *p << endl;
-  cout << *q << endl;
   cout << *r << endl;
 
   return 0;
