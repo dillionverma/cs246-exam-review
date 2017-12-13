9d87aca1bd9be4f7115bd8ed99d47a9ff7dca5a4
Dillion Verma <dillionverma@hotmail.com>
2017-12-13 00:24:55 -0500

    add range iterator pattern

---
+++ unit4/design_patterns/iterator_pattern/range_iterator.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Range {
+  int start, finish;
+
+  public: 
+    Range(int a, int b):start{a}, finish{b}{}
+    class Iterator {
+        int n;
+        Iterator(int n):n{n}{}
+      public:
+        Iterator &operator++(){ ++n; return *this; }
+        bool operator!=(const Iterator &rhs) const { return n != rhs.n; }
+        int operator*() { return n; }
+        friend class Range;
+    };
+
+    Iterator begin() { return Iterator(start);  }
+    Iterator end()   { return Iterator(finish); }
+
+    ~Range(){};
+};
+
+
+int main() {
+
+  for (auto a:Range{0,10}) cout << a << ' ';
+  cout << endl;
+
+  return 0;
+}
